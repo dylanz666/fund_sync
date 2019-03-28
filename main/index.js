d727e08c0cd240d6b7f22a44228dc4806e9f1988
dylanz <zzh9098-=>
2019-03-28 11:07:38 +0800

    set up project

--- main/index.js
+++ main/index.js
@@ -16,6 +16,6 @@ new CronJob('0,20,40 * * * * 1,2,3,4,5', async function () {
     let stokeData = await syncStokeMarketData(['0000011', '3990012', '3990052', '3990062']);//上证指数,深证成指,中小板指,创业板指,恒生指数
     //stokeData.length > 0 ? await saveDataToXlsx('stokeHistory', stokeData) : null;
 
-    let fundData = await syncFundData(['001630', '001632', '001593', '501006', '002611']);
+    let fundData = await syncFundData(['501006', '001632', '001630', '001593', '002611']);
     //fundData.length > 0 ? await saveDataToXlsx('fundHistory', fundData) : null;
 }, null, true, 'Asia/Chongqing');
